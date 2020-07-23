df37e8abda1d42284fd64279cf0c8fe8147eef37
craigmcc <craigmcc@gmail.com>
2020-07-23 15:14:04 -0700

    Save a strategy that recognizes dependencies with their own META-INF requirements, albeit requires running with a "lib" directory next to the executable jar containing all of the dependency JARs.

--- src/main/java/org/cityteam/guests/importer/Main.java
+++ src/main/java/org/cityteam/guests/importer/Main.java
@@ -15,6 +15,13 @@
  */
 package org.cityteam.guests.importer;
 
+import org.cityteam.guests.client.FacilityClient;
+import org.cityteam.guests.model.Facility;
+import org.craigmcc.library.shared.exception.BadRequest;
+import org.craigmcc.library.shared.exception.InternalServerError;
+import org.craigmcc.library.shared.exception.NotFound;
+import org.craigmcc.library.shared.exception.NotUnique;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,55 +40,20 @@ public class Main {
 
     // Accept mm/dd/yy or mm/dd/yyyy
     public static DateTimeFormatter alternateDateTimeFormatter = createDateTimeFormatter();
+    public static Facility facility;
+    public static final FacilityClient facilityClient = new FacilityClient();
     public static LocalDate fromDate;
-    public static LocalDate toDate;
+    public static String pathname;
     public static BufferedReader reader;
-    public static Scanner scanner;
-    //    public static final RegistrationClient registrationClient = new RegistrationClient();
+    public static LocalDate toDate;
 
     // Main Program ----------------------------------------------------------
 
     public static void main(String[] args) {
 
-        // Parse command line arguments
-        if (args.length != 3) {
-            System.out.println("Three arguments required:  fromDate, toDate, pathToCSVFle");
-            System.out.println("In addition, server URI is passed as system property " +
-                    PROPERTY_BASE_URI);
-            System.exit(2);
-        }
-        try {
-            fromDate = parseDate(args[0]);
-        } catch (DateTimeParseException e) {
-            System.out.println("Parsing fromDate '" + args[0] + "' failed");
-            System.exit(2);
-        }
-        try {
-            toDate = parseDate(args[1]);
-        } catch (DateTimeParseException e) {
-            System.out.println("Parsing toDate '" + args[0] + "' failed");
-            System.exit(3);
-        }
-        String pathname = args[2];
-        System.out.println("fromDate= " + fromDate.toString());
-        System.out.println("toDate=   " + toDate.toString());
-        System.out.println("pathname= " + pathname);
-        URI serverURI = null;
-        try {
-            serverURI = new URI(System.getProperty(PROPERTY_BASE_URI));
-        } catch (URISyntaxException e) {
-            System.out.println("Error parsing serverURI '" + args[2] + "': " + e.getMessage());
-            System.exit(4);
-        }
-        System.out.println("serverUri=" + serverURI);
-
-        // Set up reader
-        try {
-            reader = new BufferedReader(new FileReader(pathname));
-        } catch (FileNotFoundException e) {
-            System.out.println("Pathname '" + pathname + "' cannot be found");
-            System.exit(5);
-        }
+        processArguments(args);
+        setUpReader();
+        acquireFacility();
 
         // Read the last lines and print them
         int count = 0;
@@ -111,71 +83,150 @@ public class Main {
 
     }
 
-        // Support Methods -------------------------------------------------------
+    // Support Methods -------------------------------------------------------
 
-        /**
-         * <p>Create and return a date formatter that knows how to parse dates like "01/05/20"
-         * or "1/5/2020".</p>
-         */
-        public static DateTimeFormatter createDateTimeFormatter() {
-            // Accept two-digit or four-digit years, two digits means "20yy"
-            return DateTimeFormatter.ofPattern("M/d/[uuuu][uu]");
-        }
-
-        /**
-         * <p>Create and return a scanner appropriate for reading CSV files.</p>
-         *
-         * @param pathname Pathname to the file to be read
-         *
-         * @return A configured {@link Scanner} instance
-         *
-         * @throws FileNotFoundException if the specified pathname cannot be resolved
-         */
-        public static Scanner createScanner (String pathname) throws FileNotFoundException {
-            Scanner scanner = new Scanner(new File(pathname));
-            scanner.useDelimiter(",");
-            return scanner;
-        }
-
-        /**
-         * <p>Parse a date string using either YYYY-MM-DD or MM/DD/YY (or MM/DD/YYYY) formats.</p>
-         *
-         * @param input Input string to be parsed
-         *
-         * @return LocalDate for the parsed value
-         */
-        public static LocalDate parseDate (String input) throws DateTimeParseException {
+    /**
+     * <p>Acquire the {@link Facility} for Portland, creating one if necessary.</p>
+     */
+    public static void acquireFacility() {
+        try {
+            facility = facilityClient.findByNameExact("Portland");
+        } catch (InternalServerError e) {
+            System.out.println("Fatal error retrieving facility: " + e.getMessage());
+            System.exit(11);
+        } catch (NotFound e) {
             try {
-                return LocalDate.parse(input, DateTimeFormatter.ISO_LOCAL_DATE);
-            } catch (DateTimeParseException e) {
-                return LocalDate.parse(input, alternateDateTimeFormatter);
+                facility = facilityClient.insert(new Facility(
+                        "526 SE Grand Ave.",
+                        null,
+                        "Portland",
+                        "portland@cityteam.org",
+                        "Portland",
+                        "503-231-9334",
+                        "OR",
+                        "97214"
+                ));
+            } catch (BadRequest f) {
+                System.out.println("Data error creating facility: " + f.getMessage());
+                System.exit(12);
+            } catch (InternalServerError f) {
+                System.out.println("Fatal error creating facility: " + f.getMessage());
+                System.exit(13);
+            } catch (NotUnique f) {
+                System.out.println("Uniqueness error creating facility: " + f.getMessage());
+                System.exit(14);
             }
-
         }
+        System.out.println("facility= " + facility);
+    }
 
-        public static Line readLine() throws IOException {
-            String text = reader.readLine();
-            if (text == null) {
-                return null;
-            }
-            String[] fields = text.split(",");
-            Line line = new Line();
-            line.date = fields[0];
-            line.mat = fields[1];
-            line.first = fields[2];
-            line.last = fields[3];
-            line.type = fields[4];
-            line.comments = fields[6];
-            return line;
-        }
+    /**
+     * <p>Create and return a date formatter that knows how to parse dates like "01/05/20"
+     * or "1/5/2020".</p>
+     */
+    public static DateTimeFormatter createDateTimeFormatter() {
+        // Accept two-digit or four-digit years, two digits means "20yy"
+        return DateTimeFormatter.ofPattern("M/d/[uuuu][uu]");
+    }
 
-        static class Line {
-            public String date;
-            public String mat;
-            public String first;
-            public String last;
-            public String type;
-            public String comments;
+    /**
+     * <p>Parse a date string using either YYYY-MM-DD or MM/DD/YY (or MM/DD/YYYY) formats.</p>
+     *
+     * @param input Input string to be parsed
+     *
+     * @return LocalDate for the parsed value
+     */
+    public static LocalDate parseDate (String input) throws DateTimeParseException {
+        try {
+            return LocalDate.parse(input, DateTimeFormatter.ISO_LOCAL_DATE);
+        } catch (DateTimeParseException e) {
+            return LocalDate.parse(input, alternateDateTimeFormatter);
         }
 
     }
+
+    /**
+     * <p>Process command line arguments and set the corresponding variables.</p>
+     */
+    public static void processArguments(String[] args) {
+
+        // Parse command line arguments
+        if (args.length != 3) {
+            System.out.println("Three arguments required:  fromDate, toDate, pathToCSVFle");
+            System.out.println("In addition, server URI is passed as system property " +
+                    PROPERTY_BASE_URI);
+            System.exit(2);
+        }
+        try {
+            fromDate = parseDate(args[0]);
+        } catch (DateTimeParseException e) {
+            System.out.println("Parsing fromDate '" + args[0] + "' failed");
+            System.exit(2);
+        }
+        try {
+            toDate = parseDate(args[1]);
+        } catch (DateTimeParseException e) {
+            System.out.println("Parsing toDate '" + args[0] + "' failed");
+            System.exit(3);
+        }
+        pathname = args[2];
+        System.out.println("fromDate= " + fromDate.toString());
+        System.out.println("toDate=   " + toDate.toString());
+        System.out.println("pathname= " + pathname);
+        URI serverURI = null;
+        try {
+            serverURI = new URI(System.getProperty(PROPERTY_BASE_URI));
+        } catch (URISyntaxException e) {
+            System.out.println("Error parsing serverURI '" + args[2] + "': " + e.getMessage());
+            System.exit(4);
+        }
+        System.out.println("serverUri=" + serverURI);
+
+    }
+
+    /**
+     * <p>Read a single line from the input reader, and parse out the fields we need
+     * as strings.</p>
+     *
+     * @return {@link Line} containing parsed fields, or <code>null</code> for EOF
+     *
+     * @throws IOException If an I/O exception occurs
+     */
+    public static Line readLine() throws IOException {
+        String text = reader.readLine();
+        if (text == null) {
+            return null;
+        }
+        String[] fields = text.split(",");
+        Line line = new Line();
+        line.date = fields[0];
+        line.mat = fields[1];
+        line.first = fields[2];
+        line.last = fields[3];
+        line.type = fields[4];
+        line.comments = fields[6];
+        return line;
+    }
+
+    /**
+     * <p>Set up a reader to process the incoming lines.</p>
+     */
+    public static void setUpReader() {
+        try {
+            reader = new BufferedReader(new FileReader(pathname));
+        } catch (FileNotFoundException e) {
+            System.out.println("Pathname '" + pathname + "' cannot be found");
+            System.exit(5);
+        }
+    }
+
+    static class Line {
+        public String date;
+        public String mat;
+        public String first;
+        public String last;
+        public String type;
+        public String comments;
+    }
+
+}
